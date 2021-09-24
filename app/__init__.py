cf916d6edb39efd1c538cb4e854a8826990fdf19
Nikon Podgorny <podgorny.nikon@yandex.ru>
2021-09-24 21:52:10 +0300

    init commit. docker. tests examples

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_object('app.config.Config')
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from .index import index as index_blueprint
+    app.register_blueprint(index_blueprint)
+
+    return app
