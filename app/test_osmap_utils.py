c213afe6bd85a569c5cd7955cf54348a50aab204
Nikon Podgorny <podgorny.nikon@yandex.ru>
2021-09-25 02:05:48 +0300

    test enexpected behavior

--- app/test_osmap_utils.py
+++ app/test_osmap_utils.py
@@ -17,37 +17,3 @@ def test_query_none():
     searcher = osmap_utils.StreetSearcher(city=city)
     result = searcher.query(street)
     assert result.displayName() == None
-
-
-def test_query_check_some_names_json():
-    city = "Москва"
-    street = "Ленинградское шоссе"
-
-    some_display_names = ["Ленинградское шоссе, Молжаниновский, Молжаниновский район, Москва, Центральный федеральный округ, 141408, Россия",
-                          "Ленинградское шоссе, посёлок Сокол, Сокол, район Сокол, Москва, Центральный федеральный округ, 125493, Россия",
-                          "Ленинградское шоссе (дублёр), посёлок Сокол, Сокол, район Сокол, Москва, Центральный федеральный округ, 125493, Россия"]
-
-    searcher = osmap_utils.StreetSearcher(city=city)
-    result = searcher.query_json(street)
-
-    all_names = [part["display_name"] for part in result]
-
-    for some in some_display_names:
-        assert some in all_names
-
-
-# def test_query_check_some_points_json():
-#     city = "Москва"
-#     street = "Пятницкая улица"
-
-#     some_points = [(56.1861292, 36.9573638),
-#                    (55.872695, 37.3175797),
-#                    (55.7324561, 37.6271598)]
-
-#     searcher = osmap_utils.StreetSearcher(city=city)
-#     result = searcher.query_cords(street)
-
-#     all_points = [point.get_cord() for point in result]
-
-#     for some in some_points:
-#         assert some in all_points
