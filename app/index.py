b7bd9d7af46fc72be33c2cb57f1ee7c023e3a1a4
Nikon Podgorny <podgorny.nikon@yandex.ru>
2021-09-25 15:23:04 +0300

    htmls

--- app/index.py
+++ app/index.py
@@ -1,25 +1,8 @@
 from flask import Blueprint, render_template
-from . import db
-from .models import SomeValue
-import random
 
 index = Blueprint('index', __name__)
 
 
 @index.route('/')
 def index_endpoint():
-    r = "values:<br/>"
-    db_cont = SomeValue.query.all()
-    for e in db_cont:
-        r += e.__repr__() + "<br/>"
-
-    return r
-
-
-@index.route('/add_value/<string:val>')
-def add_value(val):
-    new_entry = SomeValue(str_value=val, random_int=random.randint(0, 42))
-    db.session.add(new_entry)
-    db.session.commit()
-
-    return "ok"
+    return render_template("index.html")
